c4c813c1adcce4abb8a1776d2943e75a7894f154
Douglas <douglasrdsousa1@gmail.com>
2020-10-15 20:01:41 -0300

    feat: initial project structure

---
+++ src/pages/404.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function NotFoundPage(): JSX.Element {
+  return (
+    <div>
+      <h1>404: Not Found</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </div>
+  );
+}
+
