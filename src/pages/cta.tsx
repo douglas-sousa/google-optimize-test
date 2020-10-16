711a8e761960828ea365502bd4d5acca1d5f8075
Douglas <douglasrdsousa1@gmail.com>
2020-10-15 22:45:30 -0300

    feat: new test

--- src/pages/cta.tsx
+++ src/pages/cta.tsx
@@ -1,18 +1,20 @@
 import React from "react";
 import { Link } from "gatsby";
-import { Button, StaticHeader } from '@/components';
+import { Button, StaticHeader, Layout } from '@/components';
 
 import '@/assets/style/pages/cta.css';
 
 export default function CTA(): JSX.Element {
   return (
-    <div id="cta">
-      <h1>Página para CTA</h1>
-      <StaticHeader />
-      <footer>
-        <Button mode="dark">Saiba mais</Button>
-        <Link to="/">Volta ao login</Link>
-      </footer>
-    </div>
+    <Layout>
+      <div id="cta">
+        <h1>Página para CTA</h1>
+        <StaticHeader />
+        <footer>
+          <Button mode="dark">Saiba mais</Button>
+          <Link to="/">Volta ao login</Link>
+        </footer>
+      </div>
+    </Layout>
   );
 }
