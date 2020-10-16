e959da1808c0acc1288008595b38d786d733a6de
Douglas <douglasrdsousa1@gmail.com>
2020-10-15 22:27:29 -0300

    feat: new test

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,17 +2,15 @@ const React = require('react');
 exports.onRenderBody = ({ setHeadComponents }) => {
   if (process.env.NODE_ENV === `production`) {
     setHeadComponents([
-      <script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=UA-174228191-1"
-      ></script>,
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-174228191-1"></script>,
       <script
         dangerouslySetInnerHTML={{
           __html: `
-         window.dataLayer = window.dataLayer || [];
-         function gtag(){dataLayer.push(arguments);}
-         gtag('js', new Date());
-         gtag('config', 'UA-174228191-1', { 'optimize_id': 'GTM-N3WQV84'});
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'UA-174228191-1', { 'optimize_id': OPT-MTS3K2S });
          `
         }}
       />
